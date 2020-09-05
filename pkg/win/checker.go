d5d7bb558b71a60d285e3a3bbe4484b51a51b0d3
Tom Elliott <tom.w.elliott@gmail.com>
2020-09-05 16:24:04 -0400

    Consolidate Checker into a single interface that could handle both Grids and Rows.

--- pkg/win/checker.go
+++ pkg/win/checker.go
@@ -1,14 +1,9 @@
 package win
 
 import (
-	"github.com/theothertomelliott/tic-tac-toverengineered/pkg/grid"
 	"github.com/theothertomelliott/tic-tac-toverengineered/pkg/player"
 )
 
 type Checker interface {
-	Winner(grid.Grid) *player.Mark
-}
-
-type RowChecker interface {
-	Winner(grid.Row) *player.Mark
+	Winner() *player.Mark
 }

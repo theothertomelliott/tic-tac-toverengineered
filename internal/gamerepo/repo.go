63c3650c2ac7308a02e932f4d31c2eebf72389d9
Tom Elliott <tom.w.elliott@gmail.com>
2020-09-20 15:13:18 -0400

    Refactor repo server

---
+++ internal/gamerepo/repo.go
@@ -0,0 +1,50 @@
+package gamerepo
+
+import (
+	context "context"
+
+	"github.com/theothertomelliott/tic-tac-toverengineered/pkg/game"
+	"github.com/theothertomelliott/tic-tac-toverengineered/pkg/game/rpcrepository"
+)
+
+func NewServer(repo game.Repository) rpcrepository.RepoServer {
+	return &repoServer{
+		repo: repo,
+	}
+}
+
+type repoServer struct {
+	repo game.Repository
+}
+
+func (r *repoServer) New(ctx context.Context, req *rpcrepository.NewRequest) (*rpcrepository.NewResponse, error) {
+	var resp *rpcrepository.NewResponse = &rpcrepository.NewResponse{}
+	id, err := r.repo.New()
+	if err != nil {
+		return nil, err
+	}
+	resp.ID = string(id)
+	return resp, nil
+}
+
+func (r *repoServer) List(ctx context.Context, req *rpcrepository.ListRequest) (*rpcrepository.ListResponse, error) {
+	resp := &rpcrepository.ListResponse{}
+	ids, err := r.repo.List(req.Max, req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	for _, id := range ids {
+		resp.ID = append(resp.ID, string(id))
+	}
+	return resp, nil
+}
+
+func (r *repoServer) Exists(ctx context.Context, req *rpcrepository.ExistsRequest) (*rpcrepository.ExistsResponse, error) {
+	resp := &rpcrepository.ExistsResponse{}
+	exists, err := r.repo.Exists(game.ID(req.ID))
+	if err != nil {
+		return nil, err
+	}
+	resp.Exists = exists
+	return resp, nil
+}

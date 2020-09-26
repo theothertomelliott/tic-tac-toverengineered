02843c4ad348de3ba8fea2bfc0859004e8d57353
Tom Elliott <tom.w.elliott@gmail.com>
2020-09-26 12:55:02 -0400

    Remove protos shell script

--- protos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build repository protos
-protoc -I pkg/game/rpcrepository/ pkg/game/rpcrepository/repo.proto --go_out=plugins=grpc:pkg/game/rpcrepository --go_opt=module=github.com/theothertomelliott/tic-tac-toeverengineered/pkg/game/rpcrepository
-
-# Build turn protos
-protoc -I pkg/turn/rpcturn/ pkg/turn/rpcturn/current.proto --go_out=plugins=grpc:pkg/turn/rpcturn --go_opt=module=github.com/theothertomelliott/tic-tac-toeverengineered/pkg/turn/rpcturn

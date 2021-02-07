b7abee1b070668def97619bcbeb7e1b60b3fa69e
Tom Elliott <tom.w.elliott@gmail.com>
2021-02-07 11:40:10 -0500

    Add extra gRPC logging

--- common/monitoring/logmonitoring/grpc.go
+++ common/monitoring/logmonitoring/grpc.go
@@ -2,6 +2,7 @@ package logmonitoring
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	"google.golang.org/grpc"
@@ -21,8 +22,13 @@ const TraceHeader string = "x-trace-headers"
 func (m *Monitoring) GRPCUnaryClientInterceptor() grpc.UnaryClientInterceptor {
 	log.Println("creating gRPC client interceptor")
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		log.Println("gRPC client call: ", method)
-		return invoker(ctx, method, req, reply, cc, opts...)
+		err := invoker(ctx, method, req, reply, cc, opts...)
+		var errMsg string
+		if err != nil {
+			errMsg = err.Error()
+		}
+		log.Printf("gRPC out: method=%q, request=%q, reply=%q, err=%q", method, renderJSON(req), renderJSON(reply), errMsg)
+		return err
 	}
 }
 
@@ -30,7 +36,15 @@ func (m *Monitoring) GRPCUnaryClientInterceptor() grpc.UnaryClientInterceptor {
 func (m *Monitoring) GRPCUnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	log.Println("creating gRPC server interceptor")
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		log.Println("gRPC server call: ", info.FullMethod)
+		log.Printf("gRPC in: method=%q, req=%q", info.FullMethod, renderJSON(req))
 		return handler(ctx, req)
 	}
+}
+
+func renderJSON(in interface{}) string {
+	data, err := json.Marshal(in)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
 }

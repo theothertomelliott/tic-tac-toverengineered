f5c0579a47bab9e3277920b18533717d598007d6
Tom Elliott <tom.w.elliott@gmail.com>
2021-05-29 14:45:41 -0400

    Increase code coverage for game results

--- services/checker/pkg/win/result_test.go
+++ services/checker/pkg/win/result_test.go
@@ -42,3 +42,75 @@ func TestFinished(t *testing.T) {
 		})
 	}
 }
+
+func TestEqual(t *testing.T) {
+	var tests = []struct {
+		name     string
+		resultA  win.Result
+		resultB  win.Result
+		expected bool
+	}{
+		{
+			name:     "incomplete game",
+			resultA:  win.Result{},
+			resultB:  win.Result{},
+			expected: true,
+		},
+		{
+			name: "same winner",
+			resultA: win.Result{
+				Winner: player.MarkToPointer(player.O),
+			},
+			resultB: win.Result{
+				Winner: player.MarkToPointer(player.O),
+			},
+			expected: true,
+		},
+		{
+			name: "different winner",
+			resultA: win.Result{
+				Winner: player.MarkToPointer(player.O),
+			},
+			resultB: win.Result{
+				Winner: player.MarkToPointer(player.X),
+			},
+			expected: false,
+		},
+		{
+			name:    "one with winner, one without winner",
+			resultA: win.Result{},
+			resultB: win.Result{
+				Winner: player.MarkToPointer(player.X),
+			},
+			expected: false,
+		},
+		{
+			name: "both draw",
+			resultA: win.Result{
+				IsDraw: true,
+			},
+			resultB: win.Result{
+				IsDraw: true,
+			},
+			expected: true,
+		},
+		{
+			name: "one draw, one not",
+			resultA: win.Result{
+				IsDraw: true,
+			},
+			resultB: win.Result{
+				IsDraw: false,
+			},
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.resultA.Equal(test.resultB)
+			if got != test.expected {
+				t.Errorf("expected %v, got %v", test.expected, got)
+			}
+		})
+	}
+}

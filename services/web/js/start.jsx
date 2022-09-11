45f137ccee82e8ad70a4f6576dab02a2eee67746
Tom Elliott <tom.w.elliott@gmail.com>
2022-09-11 01:22:29 -0400

    Use React to render the whole game UI.
    Add header with current state, with a single parent
    component rendering header, grid and handling the
    play interaction.

--- services/web/js/start.jsx
+++ services/web/js/start.jsx
@@ -4,6 +4,71 @@ var TicTacToe = require('tic_tac_toe');
 
 var api = new TicTacToe.DefaultApi(new TicTacToe.ApiClient("http://localhost:8081"));
 
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPlayer: '',
+            winner: {}
+        };
+
+        let g = this;
+        api.currentPlayer(props.id, function (error, data, response) {
+            if (error) {
+                console.error(error);
+            } else {
+                g.setState({currentPlayer: data});
+            }
+        })
+        api.winner(props.id, function (error, data, response) {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(data);
+                g.setState({winner: data});
+            }
+        })
+
+    }
+
+    render() {
+        let finished = this.state.winner.winner || this.state.winner.draw;
+        return <>
+            <GameHeader
+            winner={this.state.winner}
+            currentPlayer={this.state.currentPlayer}
+            />
+            <Grid 
+            id={this.props.id}
+            play={(x,y) => {
+                if (!finished) {
+                    // TODO: Use the API to make a move
+                    location.href = "/" + this.props.id + "/play?player=" + this.state.currentPlayer + "&pos={\"i\":" + x + ",\"j\":" + y + "}";
+                }
+            }}
+            />
+        </>
+    }
+}
+
+function GameHeader(props) {
+    var text = "";
+    if (props.winner.winner) {
+        text = "Winner: " + props.winner.winner;
+    } else if (props.winner.draw) {
+        text = "Draw!";
+    } else {
+        text = "Next Player: " + props.currentPlayer;
+    }
+    return <div className="grid grid-cols-2 my-5">
+    <a className="hover:underline justify-self-start text-blue-800 block" href="/">Home</a>
+
+    <div className="block justify-self-end">
+        {text}
+    </div>
+</div>
+}
+
 class Grid extends React.Component {
     constructor(props) {
         super(props);
@@ -14,7 +79,6 @@ class Grid extends React.Component {
                 if (error) {
                     console.error(error);
                 } else {
-                    console.log(data.grid);
                     g.setState({grid: data.grid});
                 }
             });
@@ -22,7 +86,9 @@ class Grid extends React.Component {
 
     render() {
         return <div className="grid grid-cols-3 gap-2 my-5">
-                <GridInner grid={this.state.grid}/>
+                <GridInner 
+                grid={this.state.grid}
+                play={this.props.play}/>
             </div>;
     }
 }
@@ -37,7 +103,7 @@ function GridInner(props) {
                         key={"space:" + i + "," + j}
                         content={space}
                         play={() => {
-                            doPlay(i,j);
+                           props.play(i,j);
                         }}
                     />);
             });
@@ -64,24 +130,8 @@ function GridSpace(props) {
     </div>;
 }
 
-var grid = document.getElementById("grid");
+var grid = document.getElementById("game");
 if (grid) {
     var gid = grid.dataset.gameid;
-    ReactDOM.createRoot(grid).render(<Grid id={gid}/>);
-}
-
-function doPlay(x,y) {
-    var gid = grid.dataset.gameid
-    api.currentPlayer(gid, function (error, data, response) {
-        if (error) {
-            console.error(error);
-        } else {
-            play(gid,data,x,y);
-        }
-    })
-}
-
-function play(gid,p,x,y){
-    // TODO: Handle finished
-    location.href = "/" + gid + "/play?player=" + p + "&pos={\"i\":" + x + ",\"j\":" + y + "}";
+    ReactDOM.createRoot(grid).render(<Game id={gid}/>);
 }

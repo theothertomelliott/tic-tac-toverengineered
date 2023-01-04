c7e6b4c14f601726c1eff9dc99dabf474621cd2b
Tom Elliott <tom.w.elliott@gmail.com>
2023-01-04 00:37:11 -0500

    Run a more taxing load test

--- tests/k6/test.js
+++ tests/k6/test.js
@@ -3,8 +3,8 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
+    { duration: '30s', target: 200 },
+    { duration: '1m30s', target: 100 },
     { duration: '20s', target: 0 },
   ],
 };

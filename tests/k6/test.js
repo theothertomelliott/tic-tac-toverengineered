cca4f03ed998d046afd94d8070c7fe25297f03be
Tom Elliott <tom.w.elliott@gmail.com>
2023-01-08 13:08:58 -0500

    Shorten load test

--- tests/k6/test.js
+++ tests/k6/test.js
@@ -4,7 +4,7 @@ import { check, sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '30s', target: 200 },
-    { duration: '1m30s', target: 100 },
+    { duration: '30s', target: 100 },
     { duration: '20s', target: 0 },
   ],
 };
